c9cfc5b746dc9fc543c511ead9df6e885f157fa6
Charlie <cdwhiteside@protonmail.com>
2022-07-13 23:20:56 +0100

    Add form for creating blogs and ability to save blogs to db

--- blogs/forms.py
+++ blogs/forms.py
@@ -4,4 +4,9 @@ from django import forms
 
 
 class NewBlogForm(forms.Form):
-    pass
+    
+    title = forms.CharField(max_length=200, help_text='What is the title of your blog post?')
+    strapline = forms.CharField(max_length=500, help_text='Add a strapline for further context')
+    is_encrypted = forms.BooleanField(help_text='Would you like your post asymetrically encrypted?', required=False)
+    blog_content = forms.CharField(help_text='Add your blog content here...')
+    author = forms.CharField(max_length=100, help_text='Who are you?')
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,27 @@
+from distutils.log import error
 from django.shortcuts import render
-
+from .models import ProtectedBlog
+from .forms import NewBlogForm
 # Create your views here.
+
 def nav(request):
-    return render(request, 'nav.html')
+
+    if request.method == 'POST':
+        
+        new_blog = ProtectedBlog()
+        new_blog.title = request.POST['title']
+        new_blog.strapline = request.POST['strapline']
+
+        try:
+            new_blog.is_encrypted = bool(request.POST['is_encrypted'])
+        except:
+            new_blog.is_encrypted = False
+
+        new_blog.blog_content = request.POST['blog_content']
+        new_blog.author = request.POST['author']
+        new_blog.save()
+
+        return render(request, 'success.html', {'user': 'We did it!'})
+    
+    new_form = NewBlogForm()
+    return render(request, 'nav.html', {'new_form': new_form})
