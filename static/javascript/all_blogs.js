438446a4bf5e0f8797490efdba013f4ce273bc1d
Charlie <cdwhiteside@protonmail.com>
2022-07-27 12:58:07 +0100

    add search result filtering and responsive elements

--- static/javascript/all_blogs.js
+++ static/javascript/all_blogs.js
@@ -1,19 +1,42 @@
-
-let htmlString = '';
-let toFill = document.querySelector(".grid-item-5")
-for ( let i = 0; i < newResults.length; i++) {
-    htmlString += `<a class="link-wrapper" href="{% url '1_blog' id=${newResults[i][3]} %}">
-                        <div class="blog">
-                            <div class="blog-header">
-                            <h2 class="blog-count">${newResults[i][0]}</h2>
-                            <h3 class="blog-strap">${newResults[i][1]}</h3>
-                            <h4 class="blog-strap">Author: ${newResults[i][2]}</h4>
-                            <p class="blog-strap">${newResults[i][3]}...</p>
-                            <h4 class="blog-strap">${newResults[i][4]}</h4>
-                            
+function searchResults(toDisplay) {
+    let toFill = document.querySelector(".grid-item-5")
+    let htmlString = '';
+    for ( let i = 0; i < toDisplay.length; i++) {
+        
+        
+        htmlString += `<a class="link-wrapper" href="{% url '1_blog' id=${toDisplay[i][3]} %}">
+                            <div class="blog">
+                                <div class="blog-header">
+                                <h2 class="blog-count">${toDisplay[i][0]}</h2>
+                                <h3 class="blog-strap">${toDisplay[i][1]}</h3>
+                                <h4 class="blog-strap">Author: ${toDisplay[i][2]}</h4>
+                                <p class="blog-strap">${toDisplay[i][3]}...</p>
+                                <h4 class="blog-strap blog-date">${toDisplay[i][4]}</h4>
+                                
+                                </div>
                             </div>
-                        </div>
-                    </a>`
-};
+                        </a>`
+    };
 
-toFill.innerHTML = htmlString;
+    toFill.innerHTML = htmlString;
+
+}
+
+const oldFilter = document.querySelector("#oldest");
+const newFilter = document.querySelector("#newest");
+
+newFilter.addEventListener("click", () => {
+    newResults = newResults.reverse();
+    console.log(newResults);
+    searchResults(newResults);
+})
+
+oldFilter.addEventListener("click", () => {
+    newResults = newResults.reverse();
+    console.log(newResults);
+    searchResults(newResults);
+});
+
+
+
+searchResults(newResults);
